7b883cfed86b67f2e7c37a13a65e7484aec78d8c
Vicente <vmonfort@florida-uni.es>
2023-05-21 11:30:23 +0200

    Eliminando funcionalidad correo

--- index.php
+++ index.php
@@ -10,18 +10,5 @@
         <h2>Resultado a la consulta</h2>
         <?php include 'main.php'; ?>
     </div>
-
-    <form method="post" action="correo.php">
-        <label for="destinatario">Destinatario:</label>
-        <input type="email" name="destinatario" id="destinatario" required><br><br>
-        <label for="asunto">Asunto:</label>
-        <input type="text" name="asunto" id="asunto" required><br><br>
-        <label for="mensaje">Mensaje:</label><br>
-        <textarea name="mensaje" id="mensaje" rows="5" cols="30" required></textarea><br><br>
-        <input type="submit" value="Enviar correo">
-    </form>
-
-</form>
-
 </body>
 </html>
